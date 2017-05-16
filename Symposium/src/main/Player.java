e22f6bb7f7307b2b655eaec0ac4a89c50728e1fd
jzheng7187 <jzheng7187@bths.edu>
2017-05-16 12:24:22 -0400

    yeah...

--- Symposium/src/main/Player.java
+++ Symposium/src/main/Player.java
@@ -3,15 +3,26 @@ package main;
 public class Player {
 	
 	private static int hpoints;
+	private boolean isAlive;
 	
 	public static void main(String[] args){
 		hpoints = 100;
+		isAlive = true;
 	}
 	
 	public void hit(){
 		if(//make contact
 			){
 			hpoints -= (Math.random() * 5) + 1;
+		}if(hpoints == 0){
+			isAlive = false;
 		}
 	} 
+	
+	public void death(){
+		if(isAlive == false){
+			game.GameOver();
+			//change screen to ending screen
+		}
+	}
 }
